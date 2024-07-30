13d008e53ae8be527e87cc9279b636d6343d824f
Shulammite Odde <shullyd7@gmail.com>
2024-07-30 22:48:53 +0100

    Updated Recommender Logic and UI

--- recommender/views.py
+++ recommender/views.py
@@ -66,8 +66,18 @@ class RecipeRecommendationView(View):
         for recipe in recipes:
             if recipe.local_name not in unique_recipes:
                 unique_recipes.add(recipe.local_name)
-                search_name_for_search = recipe.search_name.replace(',', '+')
-                image_url = self.fetch_image_url(search_name_for_search)
+
+                # Check if the image URL already exists in the database
+                if recipe.image_url:
+                    image_url = recipe.image_url
+                else:
+                    search_name_for_search = recipe.search_name.replace(',', '+')
+                    image_url = self.fetch_image_url(search_name_for_search)
+                    if image_url:
+                        # Save the image URL to the database
+                        recipe.image_url = image_url
+                        recipe.save()
+
                 suitable_recipes.append({
                     "recipe_name": recipe.local_name,
                     "image_url": image_url,
@@ -78,6 +88,7 @@ class RecipeRecommendationView(View):
             return render(request, 'no_recipes.html')  # Render a template for no recipes found
 
         return render(request, 'recipe_results.html', {"recipes": suitable_recipes})  # Render the results template with recipes
+
 
 
 class RateRecipeView(APIView):
